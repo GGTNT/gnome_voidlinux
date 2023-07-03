417679f076c166f0ecbff7d38145407f39aa640a
TNT <86717030+GGTNT@users.noreply.github.com>
2023-07-03 20:50:07 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo xbps-install -Su
+sudo xbps-install ssdm xorg gnome dbus pipewire alsa-utils alsa-tools apulse bluez sox flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo network-manager-applet
+flatpak install flathub com.brave.Browser
+sudo ln -s /etc/sv/alsa /var/service/
+sudo ln -s /etc/sv/dbus /var/service/
+sudo ln -s /etc/sv/gdm /var/service/
+sudo ln -s /etc/sv/bluetoothd /var/service
+sudo reboot
