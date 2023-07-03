4675ec165ea19623256be2790e8bc3e532f510d5
TNT <86717030+GGTNT@users.noreply.github.com>
2023-07-03 20:50:50 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo xbps-install -Su
-sudo xbps-install ssdm xorg gnome dbus pipewire alsa-utils alsa-tools apulse bluez sox flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo network-manager-applet
+sudo xbps-install ssdm xorg gnome dbus pipewire alsa-utils alsa-tools apulse bluez sox flatpak network-manager-applet
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.brave.Browser
 sudo ln -s /etc/sv/alsa /var/service/
 sudo ln -s /etc/sv/dbus /var/service/
