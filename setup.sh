4e1a3cd20d5adac2576704b81756b308877ae128
TNT <86717030+GGTNT@users.noreply.github.com>
2023-07-04 11:11:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 sudo xbps-install -Su
-sudo xbps-install ssdm xorg gnome dbus pipewire alsa-utils alsa-tools apulse bluez sox flatpak network-manager-applet
+sudo xbps-install gdm xorg gnome dbus pulseaudio alsa-utils alsa-tools apulse bluez sox flatpak network-manager-applet
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.brave.Browser
 sudo ln -s /etc/sv/alsa /var/service/
 sudo ln -s /etc/sv/dbus /var/service/
 sudo ln -s /etc/sv/gdm /var/service/
 sudo ln -s /etc/sv/bluetoothd /var/service
+sudo ln -s /etc/sv/pulseaudio /var/service/
+sudo usermod -aG audio votre_utilisateur
 sudo reboot
