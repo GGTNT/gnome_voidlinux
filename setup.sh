f6923fd2eda3a3b6795daacbb6c3fedfb43df9c8
TNT <86717030+GGTNT@users.noreply.github.com>
2023-10-29 22:21:45 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo xbps-install -Su
 sudo xbps-install gdm xorg dbus pipewire pipewire-pulse alsa-utils alsa-tools apulse bluez sox network-manager-applet void-repo-nonfree void-repo-multilib-nonfree void-repo-multilib
 sudo ln -s /etc/sv/alsa /var/service/
 sudo ln -s /etc/sv/dbus /var/service/
-sudo ln -s /etc/sv/lightdm /var/service/
+sudo ln -s /etc/sv/gdm /var/service/
 sudo ln -s /etc/sv/bluetoothd /var/service
 sudo ln -s /etc/sv/pipewire /var/service/
 sudo usermod -aG audio theo
