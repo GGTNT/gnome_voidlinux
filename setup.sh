26ad028db92964c7793b68140035f33b2fc7041a
TNT <86717030+GGTNT@users.noreply.github.com>
2023-11-01 15:32:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 sudo xbps-install -Su
-sudo xbps-install gdm xorg dbus pipewire pipewire-pulse alsa-utils alsa-tools apulse bluez sox network-manager-applet void-repo-nonfree void-repo-multilib-nonfree void-repo-multilib
+sudo xbps-install sddm plasma-desktop xorg dbus pulseaudio alsa-utils alsa-tools apulse bluez sox network-manager-applet void-repo-nonfree void-repo-multilib-nonfree void-repo-multilib
 sudo ln -s /etc/sv/alsa /var/service/
 sudo ln -s /etc/sv/dbus /var/service/
-sudo ln -s /etc/sv/gdm /var/service/
+sudo ln -s /etc/sv/sddm /var/service/
 sudo ln -s /etc/sv/bluetoothd /var/service
-sudo ln -s /etc/sv/pipewire /var/service/
+sudo ln -s /etc/sv/pulseaudio /var/service/
 sudo usermod -aG audio theo
