ce855ddec3353cb7239c587c05a7edde21c4d4c0
TNT <86717030+GGTNT@users.noreply.github.com>
2023-12-23 22:55:25 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
-sudo xbps-install -Su
-sudo xbps-install gdm gnome xorg dbus alsa-pipewire gstreamer-1-pipewire kpipewire-devel libjack-pipewire pipewire pipewire-devel wireplumber wireplumber-devel network-manager-applet void-repo-nonfree void-repo-multilib-nonfree void-repo-multilib xdg-user-dirs xdg-utils xdg-desktop-portal
-sudo ln -s /etc/sv/pipewire* /var/service/
-sudo ln -s /etc/sv/wireplumber /var/service/
+sudo xbps-install -Suv
+sudo xbps-install -Rs xdg-desktop-portal xdg-desktop-portal-gtk xdg-user-dirs xdg-user-dirs-gtk xdg-utils vlc pipewire libspa-bluetooth noto-fonts-cjk noto-fonts-emoji noto-fonts-ttf noto-fonts-ttf-extra libreoffice-writer libreoffice-calc libreoffice-impress rhythmbox neofetch ntfs-3g gimp inkscape lm_sensors wget udisks2 gvfs mtpfs gvfs-mtp gvfs-gphoto2 xtools WoeUSB xz unrar qt5-wayland ffmpeg Kooha handbrake inxi streamlink lightdm lightdm-gtk-greeter budgie-desktop xorg dbus pipewire libspa-bluetooth network-manager-applet void-repo-nonfree void-repo-multilib-nonfree void-repo-multilib git vim composer vscode zsh dbeaver 
+sudo ln -s /etc/sv/pipewire /var/service/
+sudo ln -s /etc/sv/pipewire-pulse /var/service/
 sudo ln -s /etc/sv/dbus /var/service/
 sudo ln -s /etc/sv/gdm /var/service/
 sudo ln -s /etc/sv/bluetoothd /var/service
-sudo usermod -aG audio theo
+git clone https://github.com/madand/runit-services
+cd runit-services
+sudo mv psd /etc/sv/
+sudo ln -s /etc/sv/psd /var/service/
+sudo chmod +x etc/sv/psd/*
+sudo usermod -aG _pipewire,pulse,pulse-access $USER
